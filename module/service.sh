b284fac2577a51dea788cc9e3148ae3b2fca2466
fatalcoder524 <11532648+fatalcoder524@users.noreply.github.com>
2026-07-01 03:22:30 +0530

    Add fq_pie qdisc support

--- module/service.sh
+++ module/service.sh
@@ -98,6 +98,7 @@ set_qdisc() {
         pfifo_fast)  		qdisc_args="pfifo_fast" ;;
         cake)        		qdisc_args="cake besteffort triple-isolate wash" ;;
         pie)         		qdisc_args="pie target 15ms tupdate 15ms alpha 2 beta 20 ecn" ;;
+		fq_pie)      		qdisc_args="fq_pie limit 2000 target 15ms tupdate 15ms alpha 2 beta 20 ecn" ;;
     esac
 
 	if [ -n "$handle_flag" ]; then
