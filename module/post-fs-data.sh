732942c7177a6146df2e75295141456482452e99
fatalcoder524 <11532648+fatalcoder524@users.noreply.github.com>
2026-07-01 04:05:57 +0530

    Optimize sysctl parameters for low latency, high throughput and stability

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -32,6 +32,7 @@ echo 16777216 > /proc/sys/net/core/rmem_max 2>/dev/null
 echo 16777216 > /proc/sys/net/core/wmem_max 2>/dev/null
 echo 4096 > /proc/sys/net/ipv4/tcp_max_syn_backlog 2>/dev/null
 echo 0 > /proc/sys/net/ipv4/tcp_mtu_probing 2>/dev/null
+echo 16384 > /proc/sys/net/ipv4/tcp_notsent_lowat 2>/dev/null
 
 # IPv6 TCP tuning
 echo 1 > /proc/sys/net/ipv6/tcp_ecn 2>/dev/null
@@ -43,9 +44,21 @@ echo 100000 > /proc/sys/net/core/netdev_max_backlog 2>/dev/null
 echo 3 > /proc/sys/net/ipv4/tcp_fastopen 2>/dev/null
 echo 917504 > /proc/sys/net/core/rmem_default 2>/dev/null
 echo 917504 > /proc/sys/net/core/wmem_default 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_autocorking 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_fack 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_dsack 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_sack 2>/dev/null
+echo 0 > /proc/sys/net/ipv4/tcp_collapse_max_bytes 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_recovery 2>/dev/null
 
 # High-Yield Network Jitter & Continuity Tweaks
 echo 0 > /proc/sys/net/ipv4/tcp_slow_start_after_idle 2>/dev/null
 echo 1 > /proc/sys/net/ipv4/tcp_no_metrics_save 2>/dev/null
 echo 204800 > /proc/sys/net/core/optmem_max 2>/dev/null
-echo "524288 786432 1048576" > /proc/sys/net/ipv4/tcp_mem 2>/dev/null
+echo 10000 > /proc/sys/net/ipv4/tcp_rto_max 2>/dev/null
+echo 2 > /proc/sys/net/ipv4/tcp_early_retrans 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_thin_linear_timeouts 2>/dev/null
+echo 1 > /proc/sys/net/ipv4/tcp_thin_dupack 2>/dev/null
+echo 300 > /proc/sys/net/ipv4/tcp_keepalive_time 2>/dev/null
+echo 15 > /proc/sys/net/ipv4/tcp_keepalive_intvl 2>/dev/null
+echo 5 > /proc/sys/net/ipv4/tcp_keepalive_probes 2>/dev/null
